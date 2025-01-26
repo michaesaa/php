62b3e8a0ba984adad9ee6d2796ca1054516fdef8
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2025-01-25 23:54:17 -0500

    modificar el texto dentro del textarea

--- editar.php
+++ editar.php
@@ -1,21 +1,13 @@
 <?php
 
-// obtener datos por la url 
-$_GET['texto'];
+$archivo = 'texto.txt';
+
+file_put_contents($archivo,$_GET['texto']);
+
+$texto = file_get_contents($archivo);
 
 
 
-// leer archivo de textp
-
-$texto = file_get_contents('texto.txt');
-
-echo $texto;
-
-// para modificarlo 
-
-$textoNuevo = file_put_contents('texto.txt', 'texto modificado');
-
-echo $textoNuevo;
 ?>
 
 <h1>Editar archivo de texto</h1>
