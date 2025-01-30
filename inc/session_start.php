6c182b9d05c1c7e59dd5197319b06cab8aee0ed4
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2025-01-30 15:02:18 -0500

    inicio de carpetas para la vista y creacion de variable [vista]

---
+++ inc/session_start.php
@@ -0,0 +1,5 @@
+<?php
+session_name("Inventario");
+session_start();
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+
+<?php
+  require_once "./inc/session_start.php";
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,9 +13,31 @@
 
 <body>
   <?php
-  include "./inc/navbar.php";
+  // si la variable get no esta definida o esta sin valor 
+  // le doy un valor por defecto a la variable en este caso login 
+  if (!isset($_GET['vista']) || $_GET['vista'] == "") {
+    $_GET['vista'] = "login";
+  }
 
-  include "./inc/script.php";
+  // para saber si hay un archivo con el mismo nombre de la variable
+  if (is_file("./vistas/" . $_GET['vista'] . ".php") &&
+    $_GET['vista'] != "login" && $_GET['vista'] != "404"
+  ) {
+    include "./inc/navbar.php";
+
+    include "./vistas/".$GET['vista'].".php";
+
+    include "./inc/script.php";
+  } else {
+      if($_GET['vista'] == "login"){
+        include "./vistas/login.php";
+      }else{
+        include "./vistas/404.php";
+
+      }
+  }
+
+ 
   ?>
 </body>
 
